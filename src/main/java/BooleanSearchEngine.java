4269beb4659af3eab6ea8681476a7c62a2ae88a9
GeorgeMoroz1 <yura0991@mail.ru>
2023-03-25 14:43:20 +0300

    Initial commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,71 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private final Map<String, List<PageEntry>> database = new HashMap<>();
+    List<String> stopList = new ArrayList<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
-    }
+
+        for (File pdf : pdfsDir.listFiles()) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+            int countPages = doc.getNumberOfPages();
+            for (int i = 1; i <= countPages; i++) {
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
+                for (var word : words) { // перебираем слова
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+                for (String word : freqs.keySet()) {
+                    PageEntry pageEntry = new PageEntry(pdf.getName(), i, freqs.get(word));
+                    if (database.containsKey(word)) {
+                        database.get(word).add(pageEntry);
+                    } else {
+                        database.put(word, new ArrayList<>());
+                        database.get(word).add(pageEntry);
+                    }
+                }
+            }
+        }
+    }// прочтите тут все pdf и сохраните нужные данные,
+    // тк во время поиска сервер не должен уже читать файлы
+
 
     @Override
-    public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+    public List<PageEntry> search(String words) {
+        String[] request = words.toLowerCase().split("\\P{IsAlphabetic}+");
+        List<PageEntry> temporaryList = new ArrayList<>();
+        List<PageEntry> result = new ArrayList<>();
+
+        for (String newWord : request) {
+            if (database.get(newWord) != null) {
+                temporaryList.addAll(database.get(newWord));
+                temporaryList.removeAll(stopList);
+            }
+        }
+        Map<String, Map<Integer, Integer>> numberAndQuantity = new HashMap<>();
+        for (PageEntry pageEntry : temporaryList) {
+            numberAndQuantity.computeIfAbsent(pageEntry.getPdfName(), key -> new HashMap<>())
+                    .merge(pageEntry.getPage(), pageEntry.getCount(), Integer::sum);
+        }
+
+        numberAndQuantity.forEach((key, value) -> {
+            for (var temporaryPage : value.entrySet()) {
+                result.add(new PageEntry(key, temporaryPage.getKey(), temporaryPage.getValue()));
+            }
+        });
+        Collections.sort(result);
+        return result;
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,42 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class PageEntry implements Comparable<PageEntry> {
     private final String pdfName;
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return Integer.compare(o.getCount(), this.getCount());
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String jsonStringResponse = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+            return jsonStringResponse;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
